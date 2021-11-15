30566b50c63fa0a98ab339ad59c28cf577b6d8d3
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 16:04:33 -0600

    add Add_Occupation to reducer switch statement

--- src/reducers/occupationReducer.js
+++ src/reducers/occupationReducer.js
@@ -3,6 +3,8 @@ export default function occupationReducer(state = {occupations: []}, action) {
   switch (action.type) {
     case 'FETCH_OCCUPATIONS':
       return {occupations: action.payload}
+    case 'ADD_OCCUPATION':
+      return {...state, occupations: [...state.occupations, action.payload]}
     default:
       return state
     }
