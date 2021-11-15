d7887c679d988e49a90078557ccc089966a2feb4
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 16:03:52 -0600

    change Occupations component to return amount of npcs and buttons

--- src/components/Occupations.js
+++ src/components/Occupations.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Occupations = props => {
+
   return (
     <div>
       <strong>Occupations</strong>
-      {props.occupations.map(occupation => <li key={occupation.id}>{occupation.name}</li>)}
+      {props.occupations.map(occupation => {
+        return (
+          <li key={occupation.id}>
+          {occupation.name} ({occupation.npcs.length})
+          <button>View</button>
+          <button>Add an NPC</button>
+          </li>
+        )
+      })}
     </div>
   )
 }
