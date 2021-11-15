171052eef5e30031e3796054c8fa24fa00b41059
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 12:06:29 -0600

    create basic OccupationsContainer framework

---
+++ src/containers/OccupationsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class OccupationsContainer extends React.Component {
+  render() {
+    return (
+        <div>
+          OccupationsContainer
+        </div>
+    )
+  }
+}
