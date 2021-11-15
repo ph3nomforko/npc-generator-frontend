62be95416180262dc40fc2ec0fc600f7d6987c55
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 14:39:36 -0600

    add handle submit and handlechange functions

--- src/components/OccupationInput.js
+++ src/components/OccupationInput.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { addOccupation } from '../actions/addOccupation'
 
-export default class OccupationInput extends React.Component {
+class OccupationInput extends React.Component {
 
   state = {
     name: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addOccupation(this.state)
   }
 
   render() {
     return (
       <div>
         <strong>Add an NPC Occupation/Role!</strong>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Name: </label>
-          <input type="text" placeholder="Name" value={this.state.name}/><br/>
+          <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
           <input type="submit" />
         </form>
       </div>
     )
   }
-  
+
 }
+
+export default connect(null, {addOccupation})(OccupationInput)
