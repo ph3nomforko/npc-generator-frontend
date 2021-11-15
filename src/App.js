11453bef39a633d5c34d4c121b855bed77938623
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 09:07:07 -0600

    add test function to see fetched occupations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://[::1]:3000/api/v1/occupations')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render () {
     return (
       <div className="App">
+        App
       </div>
     );
   }
