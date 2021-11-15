8ecb43ca3b17252d75d33fe0678f54b422a0552d
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 12:16:32 -0600

    import and render Occupations and OccupationsInput

--- src/containers/OccupationsContainer.js
+++ src/containers/OccupationsContainer.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Occupations from '../components/Occupations'
+import OccupationsInput from '../components/OccupationsInput'
 
 export default class OccupationsContainer extends React.Component {
   render() {
     return (
         <div>
-          OccupationsContainer
+          <h2>OccupationsContainer</h2>
+          <Occupations />
+          <OccupationsInput />
         </div>
     )
   }
