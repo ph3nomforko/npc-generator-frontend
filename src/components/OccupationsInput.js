de545f8c32d296d1edc4a59973074aaaa94d85d9
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 12:16:13 -0600

    create class component for Occupations input

---
+++ src/components/OccupationsInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class OccupationsInput extends React.Component {
+  render() {
+    return (
+      <div>
+        OccupationInput
+      </div>
+    )
+  }
+}
