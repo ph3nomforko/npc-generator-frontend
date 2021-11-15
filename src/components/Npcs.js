321519bfe817b51db86ced3fbfc0afe53df6ad68
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 15:46:53 -0600

    create Npcs.js for future render of all Npcs

---
+++ src/components/Npcs.js
@@ -0,0 +1,29 @@
+const Npcs = (props) => {
+  return (
+    <div>
+      <table>
+        <caption><strong>NPCs</strong></caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Race</th>
+            <th>Occupation</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.occupations.map(occupation => {
+            return occupation.npcs.map(npc => {
+              return <tr key={npc.id}>
+                <td>{npc.name}</td>
+                <td>{npc.species}</td>
+                <td>{npc.occupation.name}</td>
+                <td><button>View</button></td>
+              </tr>
+            })
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
