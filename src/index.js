c2a90987a67e85c128b5b6a77bd67002ac52373a
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 10:04:42 -0600

    import redux required components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
