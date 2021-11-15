df07a909dae8a7eac16588dc472b50ea17ac29b1
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 15:46:18 -0600

    add fetch method to addOccupation

--- src/actions/addOccupation.js
+++ src/actions/addOccupation.js
@@ -1,7 +1,16 @@
 // Function to add an occupation to the API from Add Occupation Form
 
-export const addOccupation = (formData) => {
-  return (dispatch) => {
-
+export const addOccupation = formData => {
+  return dispatch => {
+    fetch('http://[::1]:3000/api/v1/occupations', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(occupation => dispatch({type: 'ADD_OCCUPATION', payload: occupation}))
   }
 }
