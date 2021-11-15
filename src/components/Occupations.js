521034029fd8b922ad73f07a7d81b1772318714c
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 16:11:03 -0600

    change list of occupations to table

--- src/components/Occupations.js
+++ src/components/Occupations.js
@@ -4,16 +4,29 @@ const Occupations = props => {
 
   return (
     <div>
-      <strong>Occupations</strong>
-      {props.occupations.map(occupation => {
-        return (
-          <li key={occupation.id}>
-          {occupation.name} ({occupation.npcs.length})
-          <button>View</button>
-          <button>Add an NPC</button>
-          </li>
-        )
-      })}
+      <table>
+        <caption><strong>Occupations</strong></caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>NPC Count</th>
+            <th>View NPCs</th>
+            <th>Add an NPC</th>
+          </tr>
+        </thead>
+        <tbody>
+        {props.occupations.map(occupation => {
+          return (
+            <tr key={occupation.id}>
+              <td>{occupation.name}</td>
+              <td style={{textAlign:'center'}}>{occupation.npcs.length}</td>
+              <td style={{textAlign:'center'}}><button>View</button></td>
+              <td style={{textAlign:'center'}}><button>Add</button></td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
     </div>
   )
 }
