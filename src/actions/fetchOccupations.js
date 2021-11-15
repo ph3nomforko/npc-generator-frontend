adf660c170007a4afb199519c99ac7bc74457b90
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 12:26:13 -0600

    export fetchOccupations as component

--- src/actions/fetchOccupations.js
+++ src/actions/fetchOccupations.js
@@ -1,7 +1,5 @@
-const fetchOccupations = () => {
+export const fetchOccupations = () => {
   fetch('http://[::1]:3000/api/v1/occupations')
   .then(response => response.json())
   .then(data => console.log(data))
 }
-
-export default fetchOccupations;
