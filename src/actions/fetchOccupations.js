2f7e9e260025347d59dc2939b3994485d5576763
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 13:24:45 -0600

    add dispatch type and payload

--- src/actions/fetchOccupations.js
+++ src/actions/fetchOccupations.js
@@ -1,7 +1,12 @@
+// Functional component to fetch occupations from database.
+
 export const fetchOccupations = () => {
   return (dispatch) => {
     fetch('http://[::1]:3000/api/v1/occupations')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(occupations => dispatch({
+      type: 'FETCH_OCCUPATIONS',
+      payload: occupations
+    }))
   }
 }
