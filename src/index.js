716f6aa8f90c1644ee7d1c00397d5416147003bf
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 10:28:05 -0600

    begin implementation of redux store into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compoose;
+
+let myStore = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
-    <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
