21b8e54aa847ed9ca8064e24cee252a8a9d952db
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 14:16:07 -0600

    add input form and initial state to OccupationInput

--- src/components/OccupationInput.js
+++ src/components/OccupationInput.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 export default class OccupationInput extends React.Component {
+
+  state = {
+    name: ''
+  }
+
   render() {
     return (
       <div>
-        OccupationInput
+        <strong>Add an NPC Occupation/Role!</strong>
+        <form>
+          <label>Name: </label>
+          <input type="text" placeholder="Name" value={this.state.name}/><br/>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
+  
 }
