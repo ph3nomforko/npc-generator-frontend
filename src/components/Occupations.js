ca395c4eaf0d2c2f51e8ee4a27be8887eba8f694
Codi Kraus <codi.kraus@gmail.com>
2021-11-15 13:48:57 -0600

    create rendering table for NPCs

--- src/components/Occupations.js
+++ src/components/Occupations.js
@@ -3,7 +3,29 @@ import React from 'react';
 const Occupations = (props) => {
   return (
     <div>
-      Occupations
+      <table>
+        <caption><strong>NPCs</strong></caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Race</th>
+            <th>Occupation</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.occupations.map(occupation => {
+            return occupation.npcs.map(npc => {
+              return <tr key={npc.id}>
+                <td>{npc.name}</td>
+                <td>{npc.species}</td>
+                <td>{npc.occupation.name}</td>
+                <td><button>View</button></td>
+              </tr>
+            })
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
