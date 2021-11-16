c79cfdf47c749e46dbd90adde4acdba31dc154df
Codi Kraus <codi.kraus@gmail.com>
2021-11-16 10:11:46 -0600

    add flavor text to create OccupationInput

--- src/components/OccupationInput.js
+++ src/components/OccupationInput.js
@@ -26,6 +26,7 @@ class OccupationInput extends React.Component {
     return (
       <div>
         <strong>Add an NPC Occupation/Role!</strong>
+        <p>What is your NPC's role in the world? Are they a king? Or a beggar? Or perhaps the BBEG's right-hand-man?</p>
         <form onSubmit={this.handleSubmit}>
           <label>Name: </label>
           <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
