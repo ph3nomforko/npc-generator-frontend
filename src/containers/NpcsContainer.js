39b33c8672f5b6ba948e904eb21ac1a335d088ea
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 12:20:15 -0600

    import route for future routing in container

--- src/containers/NpcsContainer.js
+++ src/containers/NpcsContainer.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import NpcInput from '../components/NpcInput'
 import Npcs from '../components/Npcs'
+import Npc from '../components/Npc'
+//import { Route, Switch } from 'react-router-dom'
 
 class NpcsContainer extends React.Component {
 
   render() {
     return (
       <div>
-        A Wild NPC Container
-        <Npcs npcs={this.props.occupation.npcs}/>
-        <NpcInput />
+          <Npcs npcs={this.props.occupation.npcs} />
+          <Npc npc={this.props.occupation.npcs} />
+          <NpcInput />
       </div>
     )
   }
