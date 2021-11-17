98cfbc51bf60562032ddf02468b278bb86b1222b
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 10:58:51 -0600

    remove table from occupations and move to Npcs component

--- src/components/Occupation.js
+++ src/components/Occupation.js
@@ -8,35 +8,8 @@ const Occupation = props => {
   if (occupation) {
     return (
       <div>
-        <table>
-          <caption>
-            <h3>{occupation.name}</h3>
-            <strong>NPCs</strong>
-          </caption>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Race</th>
-              <th>Alignment</th>
-              <th>Appearance</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-          {occupation.npcs.map(npc => {
-              return (
-                <tr key={npc.id}>
-                  <td>{npc.name}</td>
-                  <td style={{textAlign:'center'}}>{npc.alignment}</td>
-                  <td style={{textAlign:'center'}}>{npc.appearance}</td>
-                  <td style={{textAlign:'center'}}><button>View</button></td>
-                </tr>
-              )})
-            }
-          </tbody>
-        </table>
-        <NpcsContainer />
-        <button>Add an NPC</button>
+        <h3>{occupation.name}</h3>
+        <NpcsContainer occupation={occupation} />
       </div>
     )
   } else {
