1de740d1cea36756bc404324b6b46412427153a5
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 12:45:25 -0600

    begin implementation of ADD_NPC switch in reducer

--- src/reducers/occupationReducer.js
+++ src/reducers/occupationReducer.js
@@ -1,10 +1,13 @@
 // Reducer for Occupations
 export default function occupationReducer(state = {occupations: []}, action) {
+  debugger
   switch (action.type) {
     case 'FETCH_OCCUPATIONS':
       return {occupations: action.payload}
     case 'ADD_OCCUPATION':
       return {...state, occupations: [...state.occupations, action.payload]}
+    case 'ADD_NPC':
+      return {}
     default:
       return state
     }
