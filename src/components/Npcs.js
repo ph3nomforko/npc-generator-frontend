eaf8a90e06e6407da1aa144e702780804fa5aa40
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 12:11:23 -0600

    create aplha sort for Npcs table

--- src/components/Npcs.js
+++ src/components/Npcs.js
@@ -1,30 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Npcs = (props) => {
+
+  let npcs = props.npcs.sort((a,b) =>{
+    if (a.name < b.name) return -1
+    if (a.name > b.name) return 1
+    return 0
+  })
+
   return (
     <div>
       <table>
         <caption><strong>NPCs</strong></caption>
         <thead>
           <tr>
-            <th>Name</th>
+            <th style={{textAlign:'left'}}>Name</th>
             <th>Race</th>
             <th>Alignment</th>
             <th>View</th>
           </tr>
         </thead>
         <tbody>
-          {props.npcs.map(npc => {
-            return (
+          {npcs.map(npc => { return (
             <tr key={npc.id}>
               <td>{npc.name}</td>
               <td>{npc.species}</td>
               <td>{npc.alignment}</td>
-              <td><button>View</button></td>
-            </tr>
-          )})}
+              <td>
+                <Link to={`/occupations/${npc.occupation.id}/npcs/${npc.id}`}>
+                  <button>View</button>
+                </Link>
+              </td>
+            </tr> )})}
         </tbody>
       </table>
+      <button>Add an NPC</button>
     </div>
   )
 }
