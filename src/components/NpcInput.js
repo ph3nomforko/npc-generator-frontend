4bb6fea31ad8d9786965102bd6dd5694b2f3c637
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 10:34:35 -0600

    create class component for NpcInput

---
+++ src/components/NpcInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class NpcInput extends React.Component {
+  render() {
+    return(
+      <div>
+        NpcInput, PP: 5
+      </div>
+    )
+  }
+}
+
+export default NpcInput
