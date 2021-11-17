01d6934c499b7b0df05c4d9164085a9d2e6333ef
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 14:05:15 -0600

    create case statement for ADD_NPC

--- src/reducers/occupationReducer.js
+++ src/reducers/occupationReducer.js
@@ -1,14 +1,22 @@
 // Reducer for Occupations
 export default function occupationReducer(state = {occupations: []}, action) {
-  debugger
+
   switch (action.type) {
     case 'FETCH_OCCUPATIONS':
       return {occupations: action.payload}
     case 'ADD_OCCUPATION':
       return {...state, occupations: [...state.occupations, action.payload]}
     case 'ADD_NPC':
-      return {}
+      return {...state, occupations: state.occupations.map(occupation => {
+        debugger
+        if (occupation.id === action.payload.id) {
+          return action.payload
+        } else {
+          return occupation
+        }
+      })}
     default:
       return state
     }
+
 }
