c49b6afede747f976332ad972a22e800d79abe3d
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 16:42:46 -0600

    create Npcs routes

--- src/containers/NpcsContainer.js
+++ src/containers/NpcsContainer.js
@@ -2,16 +2,18 @@ import React from 'react'
 import NpcInput from '../components/NpcInput'
 import Npcs from '../components/Npcs'
 import Npc from '../components/Npc'
-//import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 class NpcsContainer extends React.Component {
 
   render() {
     return (
       <div>
-          <NpcInput occupation={this.props.occupation} />
-          <Npcs npcs={this.props.occupation.npcs} />
-          <Npc npc={this.props.occupation.npcs} />
+        <Switch>
+          <Route path='/occupations/:id/new' render={() => <NpcInput occupation={this.props.occupation} />} />
+          <Route path='/occupations/:id/npcs/:id' render={(routerProps) => <Npc {...routerProps} npcs={this.props.occupation.npcs} />} />
+          <Route path='/occupations/:id/' render={(routerProps) => <Npcs {...routerProps} npcs={this.props.occupation.npcs} />} />
+        </Switch>
       </div>
     )
   }
