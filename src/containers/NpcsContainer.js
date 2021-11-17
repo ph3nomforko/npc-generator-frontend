54a49412abe320d7b9a3a7b33ab823719132aa89
Codi Kraus <codi.kraus@gmail.com>
2021-11-17 10:36:26 -0600

    create class component NpcsContainer

---
+++ src/containers/NpcsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NpcInput from '../components/NpcInput'
+
+class NpcsContainer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        A Wild NPC Container
+        <NpcInput />
+      </div>
+    )
+  }
+}
+
+export default NpcsContainer
