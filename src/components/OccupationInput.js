ed25cf0968a72045460373bca1145f9321ab6fcf
Codi Kraus <codi.kraus@gmail.com>
2021-11-18 15:40:44 -0600

    import Link to try to link to different component

--- src/components/OccupationInput.js
+++ src/components/OccupationInput.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { addOccupation } from '../actions/addOccupation'
 
 class OccupationInput extends React.Component {
@@ -27,10 +28,12 @@ class OccupationInput extends React.Component {
       <div className="container">
         <h5 style={{textAlign:'center'}}>Add an NPC Occupation/Role!</h5>
         <p style={{textAlign:'center'}}>What is your NPC's role in the world? Are they a king? Or a beggar? Or perhaps the BBEG's right-hand-man?</p>
-        <form className="container" onSubmit={this.handleSubmit}>
+        <form className="container center-align" onSubmit={this.handleSubmit}>
           <label>Name: </label>
           <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
-          <button className="btn waves-effect waves-light green" type="submit">Submit</button>
+          <button className="btn waves-effect waves-light green" type="submit" onClick={this.handleRedirect}>
+            Submit
+          </button>
         </form>
       </div>
     )
