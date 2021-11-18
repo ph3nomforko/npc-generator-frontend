2d9daa17a63d9932f005c7291430aca65bec4d65
Codi Kraus <codi.kraus@gmail.com>
2021-11-18 14:02:27 -0600

    add padding to app name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = props => {
   return (
     <nav>
       <div className="nav-wrapper lime darken-4">
-        <a href="/#" className="brand-logo">NPC Generator</a>
+        <a href="/#" className="brand-logo" style={{paddingLeft: '10px'}}>NPC Generator</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><Link to='/occupations' style={{paddingRight: '10px'}}> Occupations </Link></li>
           <li><Link to='/occupations/new'> Add an Occupation </Link></li>
