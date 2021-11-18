610a14716bf70a7d54e8529049e4b44fc9d46151
Codi Kraus <codi.kraus@gmail.com>
2021-11-18 13:57:50 -0600

    remove excess <p> tags

--- src/components/Npc.js
+++ src/components/Npc.js
@@ -15,14 +15,14 @@ const Npc = props => {
     return (
       <div>
         <h5>{npc.name}</h5>
-        <p><b>Race:</b> {npc.species}</p>
-        <p><b>Class:</b> {npc.npc_class}</p>
-        <p><b>Alignment:</b> {npc.alignment}</p>
-        <p><b>Appearance:</b> {npc.appearance}</p>
-        <p><b>Strong Ability:</b> {npc.strong_ability}</p>
-        <p><b>Weak Ability:</b> {npc.weak_ability}</p>
-        <p><b>Behavior:</b> {npc.behavior}</p>
-        <p><b>Plot Key/Hook:</b> {npc.plot_key}</p>
+        <p><b>Race:</b> {npc.species}<br/>
+        <b>Class:</b> {npc.npc_class}<br/>
+        <b>Alignment:</b> {npc.alignment}<br/>
+        <b>Appearance:</b> {npc.appearance}<br/>
+        <b>Strong Ability:</b> {npc.strong_ability}<br/>
+        <b>Weak Ability:</b> {npc.weak_ability}<br/>
+        <b>Behavior:</b> {npc.behavior}<br/>
+        <b>Plot Key/Hook:</b> {npc.plot_key}</p>
         <button onClick={handleDelete}>Delete NPC</button>
         <br/><br/><NpcEdit npc={npc} />
       </div>
