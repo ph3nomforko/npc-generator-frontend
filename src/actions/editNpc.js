5bbed22545afdc9e38e32559b73bd22fd35492cd
Codi Kraus <codi.kraus@gmail.com>
2021-11-18 09:25:22 -0600

    change url, argument, and method to Patch.

--- src/actions/editNpc.js
+++ src/actions/editNpc.js
@@ -1,12 +1,16 @@
-export const editNpc = (npc, occupationId) => {
+export const editNpc = (data) => {
+
+  debugger
+  console.log(data)
 
   return (dispatch) => {
-    fetch(`http://localhost:3000/api/v1/occupations/${occupationId}/npcs`, {
-      method: 'POST',
+    fetch(`http://localhost:3000/api/v1/occupations/${data.occupationId}/npcs/${data.npcId}`, {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       },
-      body: JSON.stringify(npc)
+      method: 'PATCH',
+      body: JSON.stringify(data)
     })
     .then(response => response.json())
     .then(occupation => dispatch({type:'EDIT_NPC', payload: occupation}))
