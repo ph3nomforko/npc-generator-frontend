4192bab5038f922a6cf9f0fe1c27fb58f2492680
Codi Kraus <codi.kraus@gmail.com>
2021-11-18 13:40:18 -0600

    add materialize initialize and make NpcEdit a modal

--- src/components/NpcEdit.js
+++ src/components/NpcEdit.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { editNpc } from '../actions/editNpc'
+import M from 'materialize-css'
 
 class NpcEdit extends React.Component {
+
+  componentDidMount() {
+    M.AutoInit()
+  }
 
   state = {
     name: '',
@@ -40,42 +45,49 @@ class NpcEdit extends React.Component {
   render() {
     return(
       <div>
-        <h5>Edit this NPC</h5>
-        <form onSubmit={this.handleSubmit}>
-          <label>Name: </label>
-          <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
-          <label>Race: </label>
-          <input type="text" placeholder="Race" name="species" value={this.state.species} onChange={this.handleChange}/><br/>
-          <label>Class: </label>
-          <input type="text" placeholder="Class" name="class" value={this.state.npc_class} onChange={this.handleChange}/><br/>
-          <label>Alignment: </label>
-          <input type="text" placeholder="Alignment" name="alignment" value={this.state.alignment} onChange={this.handleChange}/><br/>
-          <label>Appearance: </label>
-          <input type="text" placeholder="Appearance" name="appearance" value={this.state.appearance} onChange={this.handleChange}/><br/>
-          <label>Strong Ability: </label>
-          <select name="strong_ability" className="browser-default" value={this.state.strong_ability} onChange={this.handleChange}>
-            <option>Strength</option>
-            <option>Dexterity</option>
-            <option>Constitution</option>
-            <option>Intelligence</option>
-            <option>Wisdom</option>
-            <option>Charisma</option>
-          </select>
-          <br/><label>Weak Ability: </label>
-          <select name="weak_ability" className="browser-default" value={this.state.weak_ability} onChange={this.handleChange}>
-            <option>Dexterity</option>
-            <option>Strength</option>
-            <option>Constitution</option>
-            <option>Intelligence</option>
-            <option>Wisdom</option>
-            <option>Charisma</option>
-          </select>
-          <br/><label>Behavior: </label>
-          <input type="text" placeholder="Behavior" name="behavior" value={this.state.behavior} onChange={this.handleChange}/><br/>
-          <label>Plot Key: </label>
-          <input type="text" placeholder="Plot Key" name="plot_key" value={this.state.plot_key} onChange={this.handleChange}/><br/>
-          <input type="submit" />
-        </form>
+        <button data-target="modal-edit" className="btn modal-trigger">Edit this NPC</button>
+        <div id="modal-edit" className="modal">
+          <form onSubmit={this.handleSubmit}>
+            <div className="modal-content">
+              <h5 style={{textAlign:"center"}}>{this.props.npc.name}</h5>
+              <label>Name: </label>
+              <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
+              <label>Race: </label>
+              <input type="text" placeholder="Race" name="species" value={this.state.species} onChange={this.handleChange}/><br/>
+              <label>Class: </label>
+              <input type="text" placeholder="Class" name="class" value={this.state.npc_class} onChange={this.handleChange}/><br/>
+              <label>Alignment: </label>
+              <input type="text" placeholder="Alignment" name="alignment" value={this.state.alignment} onChange={this.handleChange}/><br/>
+              <label>Appearance: </label>
+              <input type="text" placeholder="Appearance" name="appearance" value={this.state.appearance} onChange={this.handleChange}/><br/>
+              <label>Strong Ability: </label>
+              <select name="strong_ability" className="browser-default" value={this.state.strong_ability} onChange={this.handleChange}>
+                <option>Strength</option>
+                <option>Dexterity</option>
+                <option>Constitution</option>
+                <option>Intelligence</option>
+                <option>Wisdom</option>
+                <option>Charisma</option>
+              </select>
+              <br/><label>Weak Ability: </label>
+              <select name="weak_ability" className="browser-default" value={this.state.weak_ability} onChange={this.handleChange}>
+                <option>Dexterity</option>
+                <option>Strength</option>
+                <option>Constitution</option>
+                <option>Intelligence</option>
+                <option>Wisdom</option>
+                <option>Charisma</option>
+              </select>
+              <br/><label>Behavior: </label>
+              <input type="text" placeholder="Behavior" name="behavior" value={this.state.behavior} onChange={this.handleChange}/><br/>
+              <label>Plot Key: </label>
+              <input type="text" placeholder="Plot Key" name="plot_key" value={this.state.plot_key} onChange={this.handleChange}/><br/>
+            </div>
+            <div className="modal-footer">
+              <input className="modal-close" type="submit" />
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
